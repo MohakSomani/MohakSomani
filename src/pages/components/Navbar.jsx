5851f2b1dce2df455cfb3aaa64d9d2d14b83a34c
MohakSomani <mohaksomani@gmail.com>
2024-06-18 15:37:05 +0530

    still in progress tag added

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -6,6 +6,7 @@ function Navbar() {
       <a href="https://iiitaphyd-my.sharepoint.com/personal/mohak_somani_students_iiit_ac_in/_layouts/15/download.aspx?UniqueId=ae259b87-0996-48ce-a1eb-0bc9acd75440" className="btn-download">
         <i className="fas fa-download"></i> Download CV
       </a>
+      <h5>THE WEBSITE IS STILL IN PROGRESS..</h5>
       <div className="nav-links">
         <a href="#home" className="nav-link nav-link-active">
           HOME
