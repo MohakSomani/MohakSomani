c19acf54f5a4fc70211c88ad308a0c22b9279c19
MohakSomani <mohaksomani@gmail.com>
2024-06-20 18:44:03 +0530

    cv link changed

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="navbar">
-      <a href="https://iiitaphyd-my.sharepoint.com/personal/mohak_somani_students_iiit_ac_in/_layouts/15/download.aspx?UniqueId=ae259b87-0996-48ce-a1eb-0bc9acd75440" className="btn-download">
+      <a href="https://drive.google.com/uc?export=download&id=1zAqCkeOAgqQnyI6RpxRmmXrKcwzGgg_V" className="btn-download">
         <i className="fas fa-download"></i> Download CV
       </a>
       <h5>THE WEBSITE IS STILL IN PROGRESS..</h5>
